6ebf9ed1f826674399c94aab06530b5cf76a4198
tmccaff <iridude@aol.com>
2019-08-22 21:07:09 -0400

    Add files via upload

---
+++ includes/extra_datafiles/wishlist_filenames.php
@@ -0,0 +1,12 @@
+<?php
+
+// Wishlist
+define('FILENAME_WISHLIST', 'wishlist');
+define('FILENAME_WISHLIST_S', 'wishlist_s');
+define('FILENAME_WISHLIST_EMAIL', 'wishlist_email');
+define('FILENAME_WISHLIST_FIND', 'wishlist_find');
+define('FILENAME_WISHLIST_EDIT', 'wishlist_edit');
+define('FILENAME_WISHLISTS', 'wishlists');
+define('FILENAME_WISHLIST_MOVE', 'wishlist_move');
+
+?>
