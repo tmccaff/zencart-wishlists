fc3f1832b64693ad86ce1b6ab5806602754482a1
tmccaff <iridude@aol.com>
2019-08-24 18:53:59 -0400

    Add files via upload

---
+++ YOUR_ADMIN/includes/auto_loaders/config.wishlist_admin.php
@@ -0,0 +1,7 @@
+<?php
+// -----
+// Part of the WishLists plugin, created by Thomas McCaffery
+
+$autoLoadConfig[210][] = array ('autoType' => 'init_script',
+                                'loadFile' => 'init_wishlist_admin.php');
+
