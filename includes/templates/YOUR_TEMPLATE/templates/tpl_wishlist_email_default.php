207044558b971ffe8a7719d0dc85700804132b4f
tmccaff <iridude@aol.com>
2019-08-25 13:00:09 -0400

    Update wishlist text

--- includes/templates/YOUR_TEMPLATE/templates/tpl_wishlist_email_default.php
+++ includes/templates/YOUR_TEMPLATE/templates/tpl_wishlist_email_default.php
@@ -1,5 +1,4 @@
-<div id="wishlist"> <!-- begin wishlist id for styling -->
-
+<div id="wishlist"> 
 <h1><?php echo HEADING_TITLE; ?></h1>
 
 <p><?php echo TEXT_DESCRIPTION; ?></p>
@@ -10,14 +9,14 @@
 
 <p class="inputrequirement"><?php echo FORM_REQUIRED_INFORMATION; ?></p>
 
-<?php echo zen_draw_form('wishlist_email', zen_href_link(UN_FILENAME_WISHLIST_EMAIL, 'action=process', 'SSL')); ?>
+<?php echo zen_draw_form('wishlist_email', zen_href_link(FILENAME_WISHLIST_EMAIL, 'action=process', 'SSL')); ?>
 <?php echo zen_draw_hidden_field('wid', $id); ?>
 <fieldset>
 	<legend><?php echo FORM_TITLE; ?></legend>
 	<div class="group">
 
 	<div class="formrow">
-		<label class="block" for="to_email_address"><?php echo FORM_LABEL_EMAIL . ' <span class="inputrequirement">' . UN_TEXT_FORM_FIELD_REQUIRED . '</span>'; ?></label>
+		<label class="block" for="to_email_address"><?php echo FORM_LABEL_EMAIL . ' <span class="inputrequirement">' . TEXT_FORM_FIELD_REQUIRED . '</span>'; ?></label>
 		<?php echo zen_draw_input_field('to_email_address', '', 'class="l"'); ?>
 	</div>
 	
@@ -30,7 +29,7 @@
 </fieldset>
 
 <div class="buttons">
-<?php echo '<a href="' . zen_href_link(UN_FILENAME_WISHLIST, zen_get_all_get_params(), 'SSL') . '">' . zen_image_button(BUTTON_IMAGE_BACK, BUTTON_BACK_ALT) . '</a>'; ?>
+<?php echo '<a href="' . zen_href_link(FILENAME_WISHLIST, zen_get_all_get_params(), 'SSL') . '">' . zen_image_button(BUTTON_IMAGE_BACK, BUTTON_BACK_ALT) . '</a>'; ?>
 <?php echo zen_image_submit(BUTTON_IMAGE_TELLAFRIEND, BUTTON_TELL_A_FRIEND_ALT, 'class="button-l"'); ?>
 </div>
 
@@ -41,4 +40,5 @@
 <dd><?php echo TEXT_MESSAGE_FROM; ?></dd>
 </dl>
 
-</div> <!-- end (un) id for styling -->
+</div>
+</div> 
