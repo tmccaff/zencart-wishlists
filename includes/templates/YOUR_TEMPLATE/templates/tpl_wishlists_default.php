9c8107018f108fe10f8a02351202ba2c85694671
tmccaff <iridude@aol.com>
2019-08-25 13:12:01 -0400

    Update wishlist text

--- includes/templates/YOUR_TEMPLATE/templates/tpl_wishlists_default.php
+++ includes/templates/YOUR_TEMPLATE/templates/tpl_wishlists_default.php
@@ -1,5 +1,4 @@
-<div id="wishlist"> <!-- begin wishlist id for styling -->
-
+<div id="wishlist"> 
 <h1><?php echo HEADING_TITLE; ?></h1>
 
 <p><?php echo TEXT_DESCRIPTION; ?></p>
@@ -11,9 +10,9 @@ if ( $messageStack->size('wishlists') > 0 ) {
 ?>
 
 <ul>
-	<li><a href="<?php echo zen_href_link(UN_FILENAME_WISHLIST_FIND, '', 'SSL'); ?>"><?php echo UN_TEXT_FIND_WISHLIST; ?></a></li>
-	<?php if ( UN_ALLOW_MULTIPLE_WISHLISTS===true ) { ?>
-	<li><a href="<?php echo zen_href_link(UN_FILENAME_WISHLIST_EDIT, 'op=add', 'SSL'); ?>"><?php echo UN_TEXT_NEW_WISHLIST; ?></a></li>
+	<li><a href="<?php echo zen_href_link(FILENAME_WISHLIST_FIND, '', 'SSL'); ?>"><?php echo TEXT_FIND_WISHLIST; ?></a></li>
+	<?php if ( ALLOW_MULTIPLE_WISHLISTS===true ) { ?>
+	<li><a href="<?php echo zen_href_link(FILENAME_WISHLIST_EDIT, 'op=add', 'SSL'); ?>"><?php echo TEXT_NEW_WISHLIST; ?></a></li>
 	<?php } ?>
 </ul>
  
@@ -31,39 +30,39 @@ if ( $messageStack->size('wishlists') > 0 ) {
 		}
 ?>
 
-		<div class="wishlist<?php echo (!un_is_empty($tdclass)? '-'.$tdclass: ''); ?>">
+		<div class="wishlist<?php echo (!is_empty($tdclass)? '-'.$tdclass: ''); ?>">
 		
 		<!-- buttons -->
-		<a href="<?php echo zen_href_link(UN_FILENAME_WISHLIST_EDIT, 'wid='.$records->fields['id'].'&op=edit', 'SSL'); ?>" title="<?php echo BUTTON_EDIT_SMALL_ALT; ?>"><?php echo zen_image_button(BUTTON_IMAGE_EDIT_SMALL, BUTTON_EDIT_SMALL_ALT, 'style="float: right; margin-left: 5px;"'); ?></a>
-		<a href="<?php echo zen_href_link(UN_FILENAME_WISHLISTS, 'wid='.$records->fields['id'].'&op=del', 'SSL'); ?>" title="<?php echo  BUTTON_DELETE_SMALL_ALT; ?>"><?php echo zen_image_button(BUTTON_IMAGE_DELETE_SMALL, BUTTON_DELETE_SMALL_ALT, 'style="float: right;"'); ?></a>
+		<a href="<?php echo zen_href_link(FILENAME_WISHLIST_EDIT, 'wid='.$records->fields['id'].'&op=edit', 'SSL'); ?>" title="<?php echo BUTTON_EDIT_SMALL_ALT; ?>"><?php echo zen_image_button(BUTTON_IMAGE_EDIT_SMALL, BUTTON_EDIT_SMALL_ALT, 'style="float: right; margin-left: 5px;"'); ?></a>
+		<a href="<?php echo zen_href_link(FILENAME_WISHLISTS, 'wid='.$records->fields['id'].'&op=del', 'SSL'); ?>" title="<?php echo  BUTTON_DELETE_SMALL_ALT; ?>"><?php echo zen_image_button(BUTTON_IMAGE_DELETE_SMALL, BUTTON_DELETE_SMALL_ALT, 'style="float: right;"'); ?></a>
 
 		<!-- data -->
-		<h3><a href="<?php echo zen_href_link(UN_FILENAME_WISHLIST, 'wid='.$records->fields['id'], 'SSL'); ?>"><?php echo $records->fields['name']; ?></a></h3>
+		<h3><a href="<?php echo zen_href_link(FILENAME_WISHLIST, 'wid='.$records->fields['id'], 'SSL'); ?>"><?php echo $records->fields['name']; ?></a></h3>
 		
 		<ul>
-		<li><?php echo TABLE_HEADING_ITEMS . UN_LABEL_DELIMITER . $records->fields['items_count']; ?> 
+		<li><?php echo TABLE_HEADING_ITEMS . LABEL_DELIMITER . $records->fields['items_count']; ?> 
 		<?php if ( $records->fields['items_count']>0 ) { ?>
 		<?php echo TEXT_ACTION_DELIMITER; ?>
-		<a href="<?php echo zen_href_link(UN_FILENAME_WISHLIST_MOVE, 'wid='.$records->fields['id'], 'SSL'); ?>"><?php echo TEXT_MOVE; ?></a>
+		<a href="<?php echo zen_href_link(FILENAME_WISHLIST_MOVE, 'wid='.$records->fields['id'], 'SSL'); ?>"><?php echo TEXT_MOVE; ?></a>
 		<?php } ?>
 		</li>
-		<li><?php echo TABLE_HEADING_COMMENT . UN_LABEL_DELIMITER . $records->fields['comment']; ?></li>
-		<li><?php echo TABLE_HEADING_DEFAULT . UN_LABEL_DELIMITER; ?>
+		<li><?php echo TABLE_HEADING_COMMENT . LABEL_DELIMITER . $records->fields['comment']; ?></li>
+		<li><?php echo TABLE_HEADING_DEFAULT . LABEL_DELIMITER; ?>
 		<?php if ( $records->fields['default_status']==1 ) { ?>
 		<?php echo TEXT_YES; ?>
 		<?php } else { ?>
 		<?php echo TEXT_NO; ?> 
 		<?php echo TEXT_ACTION_DELIMITER; ?>
-		<a href="<?php echo zen_href_link(UN_FILENAME_WISHLISTS, 'wid='.$records->fields['id'].'&op=default', 'SSL'); ?>" title="<?php echo TEXT_MAKE_DEFAULT; ?>"><?php echo TEXT_MAKE_DEFAULT; ?></a>
+		<a href="<?php echo zen_href_link(FILENAME_WISHLISTS, 'wid='.$records->fields['id'].'&op=default', 'SSL'); ?>" title="<?php echo TEXT_MAKE_DEFAULT; ?>"><?php echo TEXT_MAKE_DEFAULT; ?></a>
 		<?php } ?>
 		</li>
-		<li><?php echo TABLE_HEADING_PUBLIC . UN_LABEL_DELIMITER; ?>
+		<li><?php echo TABLE_HEADING_PUBLIC . LABEL_DELIMITER; ?>
 		<?php if ( $records->fields['public_status']==1 ) { ?>
-		<a href="<?php echo zen_href_link(UN_FILENAME_WISHLISTS, 'wid='.$records->fields['id'].'&op=deact', 'SSL'); ?>" title="<?php echo TEXT_MAKE_PRIVATE; ?>"><?php echo TEXT_YES; ?></a> 
+		<a href="<?php echo zen_href_link(FILENAME_WISHLISTS, 'wid='.$records->fields['id'].'&op=deact', 'SSL'); ?>" title="<?php echo TEXT_MAKE_PRIVATE; ?>"><?php echo TEXT_YES; ?></a> 
 		<?php echo TEXT_ACTION_DELIMITER; ?>
-		<a href="<?php echo zen_href_link(UN_FILENAME_WISHLIST_EMAIL, 'wid='.$records->fields['id'], 'SSL'); ?>" title="<?php echo UN_TEXT_EMAIL_WISHLIST; ?>"><?php echo UN_TEXT_EMAIL_WISHLIST; ?></a>
+		<a href="<?php echo zen_href_link(FILENAME_WISHLIST_EMAIL, 'wid='.$records->fields['id'], 'SSL'); ?>" title="<?php echo TEXT_EMAIL_WISHLIST; ?>"><?php echo TEXT_EMAIL_WISHLIST; ?></a>
 		<?php } else { ?>
-		<a href="<?php echo zen_href_link(UN_FILENAME_WISHLISTS, 'wid='.$records->fields['id'].'&op=act', 'SSL'); ?>" title="<?php echo TEXT_MAKE_PUBLIC; ?>"><?php echo TEXT_NO; ?></a>
+		<a href="<?php echo zen_href_link(FILENAME_WISHLISTS, 'wid='.$records->fields['id'].'&op=act', 'SSL'); ?>" title="<?php echo TEXT_MAKE_PUBLIC; ?>"><?php echo TEXT_NO; ?></a>
 		<?php } ?>
 		</li>
 		</ul>
@@ -84,4 +83,4 @@ if ( $messageStack->size('wishlists') > 0 ) {
 <?php echo zen_back_link() . zen_image_button(BUTTON_IMAGE_BACK, BUTTON_BACK_ALT) . '</a>'; ?>
 </div>
 
-</div> <!-- end (un) id for styling -->
+</div> 
